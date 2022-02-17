6894070bc8b626b908975799119de1ba205787fd
arqamshakeelarthur <95334470+arqamshakeelarthur@users.noreply.github.com>
2022-02-17 15:28:15 +0500

    WIPP

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+function App () {
+
+  function hello()
+  {
+    
+  }
   return (
     <div className="App">
       <header className="App-header">
